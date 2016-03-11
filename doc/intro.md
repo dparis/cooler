f6ce6491b850eac19bcb2465fdf660a551f25313
Dylan Paris <dylan.paris+github@gmail.com>
2016-03-11 14:21:26 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cooler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cooler/core_test.clj
@@ -0,0 +1,7 @@
+(ns cooler.core-test
+  (:require [clojure.test :refer :all]
+            [cooler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
