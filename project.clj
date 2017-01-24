99ce80601c717d91fff6832f77ae393a17012b6c
Dylan Paris <dylan.paris+github@gmail.com>
2017-01-24 14:48:45 -0800

    Set up lein deploy config to use clojars by default

--- project.clj
+++ project.clj
@@ -19,6 +19,8 @@
                  [prismatic/schema "1.1.3"]
                  [com.taoensso/timbre "4.8.0"]]
 
+  :deploy-repositories [["releases" :clojars]]
+
   :jvm-opts ["-Duser.timezone=UTC"]
 
   :profiles {:dev {:source-paths ["dev"]
